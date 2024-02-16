869d3ec8787ec31dbb0a6e70d1622ccec9f2ec9f
2h311 <kcodepal@outlook.com>
2024-02-16 23:42:31 +0100

    still working on getting things to work well before touching the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import { getSampleWords, getWordsByFrequency } from "./helpers/helperfunctions";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [wordsToCount, setWordsToCount] = useState("");
+  const [showLoadingText] = useState(
+    "Loading sample text ... Please wait for it 😊"
+  );
+
+  const countWords = () => {
+    if (wordsToCount) {
+      const wordMap = getWordsByFrequency(wordsToCount);
+      console.log(wordMap);
+    }
+  };
+
+  const loadSampleWords = async () => {
+    setWordsToCount(showLoadingText); // use this to show some loading text
+    const sampleWord = await getSampleWords();
+    setWordsToCount(sampleWord);
+  };
+
+  const getWordsFromTextField = (text) => {
+    setWordsToCount(text);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <textarea
+        name="textarea"
+        cols="100"
+        rows="10"
+        value={wordsToCount}
+        onChange={(event) => getWordsFromTextField(event.target.value)}
+      ></textarea>
+      <input type="button" value="Count words" onClick={countWords} />
+      <input type="button" value="Load sample" onClick={loadSampleWords} />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/helpers/helperfunctions.jsx
@@ -0,0 +1,48 @@
+const getRandomIntInclusive = (min, max) => {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+};
+
+const getSampleWords = async (randomNumber = getRandomIntInclusive(1, 10)) => {
+  const hipsumEndpoint = `http://hipsum.co/api/?type=hipster-centric&sentences=${randomNumber}`;
+  const response = await fetch(hipsumEndpoint);
+  let sampleWordFromHipsum;
+  if (response.ok === true && response.status === 200) {
+    sampleWordFromHipsum = await response.json();
+    sampleWordFromHipsum = sampleWordFromHipsum[0];
+  } else {
+    sampleWordFromHipsum =
+      "If you press the count button, you should see the absolute frequency of these words in a table and pie chart. If you press the load sample button instead, you should see the some lorem ipsum paragraphs. You consider it a default message.";
+  }
+  return sampleWordFromHipsum;
+};
+
+const regexWord = /\w+/g;
+const getWordsByFrequency = (text) => {
+  return (
+    text
+      // array of words
+      .match(regexWord)
+      // array of _lowercase_ words
+      .map((word) => word.toLowerCase())
+      // array describing for each word 1 objects with the word's own value and its frequency
+      .reduce((acc, curr) => {
+        const index = acc.findIndex(({ word }) => word === curr);
+        if (index === -1) {
+          return [
+            ...acc,
+            {
+              word: curr,
+              frequency: 1,
+            },
+          ];
+        }
+        acc[index].frequency += 1;
+        return acc;
+      }, [])
+      // sorted in descending order
+      .sort((a, b) => b.frequency - a.frequency)
+  );
+};
+export { getRandomIntInclusive, getSampleWords, getWordsByFrequency };
