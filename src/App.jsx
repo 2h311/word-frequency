5b2652788063437b29ca531e4ffc29a062ff35f9
2h311 <kcodepal@outlook.com>
2024-02-17 01:05:52 +0100

    removed the global messages into an export file

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,7 @@ import {
   getSampleWordsFromHipsum,
   getWordsByFrequency,
 } from "./helpers/helperfunctions";
-
-const loadingMessage = "Loading sample text ... Please wait for it 😊",
-  emptyMessage =
-    "Empty textarea\nIf you press the count button, you should see the absolute frequency of these words in a table and pie chart. If you press the load button instead, you should see the first paragraph from an episode of the freeCodeCamp podcast. Or this message. Consider it a default. This message.";
+import { loadingMessage, emptyMessage } from "./helpers/helpermessages";
 
 const App = () => {
   const [wordInTextarea, setWordInTextarea] = useState("");
---
+++ src/helpers/helpermessages.jsx
@@ -0,0 +1,3 @@
+export const loadingMessage = "Loading sample text ... Please wait for it 😊",
+  emptyMessage =
+    "Empty textarea\nIf you press the count button, you should see the absolute frequency of these words in a table and pie chart. If you press the load button instead, you should see the first paragraph from an episode of the freeCodeCamp podcast. Or this message. Consider it a default. This message.";
